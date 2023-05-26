dec2ab37933a39917d2705ad387011747925def0
Jayesh Yadav <jayeshyadav528@gmail.com>
2023-05-26 12:15:54 +0530

    Fixing Auth0 Routes

---
+++ 01-login/routes/index.js
@@ -0,0 +1,31 @@
+var router = require('express').Router();
+const { requiresAuth } = require('express-openid-connect');
+
+router.get('/', function (req, res, next) {
+  if (req.oidc.isAuthenticated()) {
+    console.log("ha");
+   const  title= 'Auth0 Webapp sample Nodejs'
+   const isAuthenticated= true
+    res.redirect(`http://localhost:5173/`); // Redirect to the home page after authentication
+  } else {
+    console.log("naaaa");
+    const  title= 'Auth0 Webapp sample Nodejs'
+   const isAuthenticated= false
+   req.oidc.user=null;
+    res.redirect(`http://localhost:5173`);
+  }
+});
+
+
+router.get('/profile', requiresAuth(), function (req, res, next) {
+  // res.render('profile', {
+  //   userProfile: JSON.stringify(req.oidc.user, null, 2),
+  //   title: 'Profile page'
+  // });
+  res.send({
+    title:"jayesh",
+    userProfile: JSON.stringify(req.oidc.user, null, 2)
+  });
+});
+
+module.exports = router;
---
+++ 01-login/server.js
@@ -0,0 +1,66 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const http = require('http');
+const logger = require('morgan');
+const path = require('path');
+const router = require('./routes/index');
+const { auth } = require('express-openid-connect');
+const cors=require('cors');
+
+dotenv.load();
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(cors())
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+const config = {
+  attemptSilentLogin: false,
+  authRequired: false,
+  idpLogout: true,
+  auth0Logout: true
+};
+
+const port = process.env.PORT || 3000;
+if (!config.baseURL && !process.env.BASE_URL && process.env.PORT && process.env.NODE_ENV !== 'production') {
+  config.baseURL = `http://localhost:${port}`;
+}
+
+app.use(auth(config));
+
+// Middleware to make the `user` object available for all views
+app.use(function (req, res, next) {
+  res.locals.user = req.oidc.user;
+  next();
+});
+
+app.get('/user', function (req, res, next) {
+  res.json(req.oidc.user);
+});
+app.use('/', router);
+
+
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Error handlers
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: process.env.NODE_ENV !== 'production' ? err : {}
+  });
+});
+
+http.createServer(app)
+  .listen(port, () => {
+    console.log(`Listening on ${config.baseURL}`);
+  });
